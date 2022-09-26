4dc6caa7c0049c03d0f1126fd6a12e9fc34d376f
ceppes <romero.diego31@gmail.com>
2022-09-27 00:03:06 +0200

    Some telescope builtin keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -84,6 +84,10 @@ map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 map('n', '<leader>fr', '<cmd>Telescope resume<cr>')
 map('n', '<leader>fp', '<cmd>Telescope pickers<cr>')
 
+map('n', '<leader>tc', "<cmd>lua require('telescope.builtin').colorscheme()<cr>")
+map('n', '<leader>tgr', "<cmd>lua require('telescope.builtin').lsp_references()<cr>")
+map('n', '<leader>tgd', "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>")
+
 -- Using Lua functions
 --map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>")
 --map('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
