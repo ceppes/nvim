1d731a843b84816d8c139eaad19ab370c19977aa
ceppes <romero.diego31@gmail.com>
2022-10-25 16:04:56 +0000

    Add nvim custom command to be able to copy content with tmux without issues

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -3,6 +3,37 @@ vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
+local copyMode = false
+vim.api.nvim_create_user_command('CopyModeToggle', function ()
+  if copyMode then
+    vim.opt.listchars:append "space:⋅"
+    vim.opt.listchars:append "eol:↴"
+    vim.opt.number = true
+    require('gitsigns').setup({current_line_blame=true})
+    copyMode = false
+  else
+    vim.opt.listchars:append "space: "
+    vim.opt.listchars:append "eol: "
+    vim.opt.number = false
+    require('gitsigns').setup({current_line_blame=false})
+    copyMode = true
+  end
+end, { nargs = 0, desc = 'Turn on indent signs' })
+
+vim.api.nvim_create_user_command('CopyModeOff', function ()
+  vim.opt.listchars:append "space:⋅"
+  vim.opt.listchars:append "eol:↴"
+  vim.opt.number = true
+  require('gitsigns').setup({current_line_blame=true})
+end, { nargs = 0, desc = 'Turn on indent signs' })
+
+vim.api.nvim_create_user_command('CopyModeOn', function ()
+  vim.opt.listchars:append "space: "
+  vim.opt.listchars:append "eol: "
+  vim.opt.number = false
+  require('gitsigns').setup({current_line_blame=false})
+end, { nargs = 0, desc =  'Turn off indent signs'})
+
 -- vim.opt.termguicolors = true
 -- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
