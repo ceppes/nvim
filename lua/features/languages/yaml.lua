ce4ec1459cbb673c2afcadff1051338323b4aa04
ceppes <romero.diego31@gmail.com>
2023-06-20 21:00:48 +0200

    Update yaml ls

--- lua/features/languages/yaml.lua
+++ lua/features/languages/yaml.lua
@@ -1,1 +1,23 @@
-require'lspconfig'.yamlls.setup {}
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#yamlls
+local M = {}
+
+M.lspbin = "yaml-language-server"
+
+function M.lsp()
+
+  return require("features.lsp.server_config").config(
+    M.lspbin,
+    {
+      settings = {
+        yaml = {
+          schemas = {
+            ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+          },
+        },
+      }
+    }
+  )
+
+end
+
+return M
