ed9c8fa276091a90830fe657be370c53223af180
ceppes <romero.diego31@gmail.com>
2023-06-26 00:13:46 +0200

    fix bug python

--- lua/features/languages/python.lua
+++ lua/features/languages/python.lua
@@ -4,38 +4,44 @@ M.linter = 'pylint'
 M.lspbin = 'pyright-langserver'
 M.debugger = 'debugpy'
 
-local util = require('lspconfig/util')
-local path = util.path
-
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
- -- Find and use virtualenv via poetry in workspace directory.
-  local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
-  if match ~= '' then
-    local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
-    return path.join(venv, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
-end
-
 function M.lsp()
   local lsp_status_ok, lspconfig = pcall(require, 'lspconfig')
   if not lsp_status_ok then
     return
+  end
+
+  local util = require('lspconfig/util')
+  local path = util.path
+
+  local function get_python_path(workspace)
+    -- print("testtttt")
+    -- print("wod " .. workspace)
+
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+      -- print("1 " .. path.join(vim.env.VIRTUAL_ENV, 'bin', 'python'))
+      return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+    end
+
+    -- Find and use virtualenv via poetry in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+    if match ~= '' then
+      local venv = vim.fn.trim(vim.fn.system('poetry env info -p'))
+      -- print("2 " .. path.join(venv, 'bin', 'python'))
+      return path.join(venv, 'bin', 'python')
+    end
+
+    -- Find and use virtualenv in workspace directory.
+    for _, pattern in ipairs({'*', '.*'}) do
+      local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+      if match ~= '' then
+        -- print("3 " .. path.join(path.dirname(match), 'bin', 'python'))
+        return path.join(path.dirname(match), 'bin', 'python')
+      end
+    end
+
+    -- Fallback to system Python.
+    return exepath('python3') or exepath('python') or 'python'
   end
 
   local python_root_files = {
@@ -79,12 +85,13 @@ function M.debugger()
   if not dap_status_ok then
     return
   end
+  -- python_path = get_python_path()
 
   dap.adapters.python = {
     type = 'executable';
-    command = function(config)
-      return get_python_path(config.root_dir)
-    end;
+    -- command = function(config)
+    --   return get_python_path(config.root_dir)
+    -- end;
     args = { '-m', 'debugpy.adapter' };
   }
 
@@ -98,9 +105,9 @@ function M.debugger()
       -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
       program = "${file}"; -- This configuration will launch the current file if used.
-      pythonPath = function(config)
-        return get_python_path(config.root_dir)
-      end;
+      -- pythonPath = function(config)
+      --   return get_python_path(config.root_dir)
+      -- end;
     },
       {
     },
@@ -113,9 +120,9 @@ function M.debugger()
         local filter = vim.fn.input('Enter unittest args: ')
         return {'-v', filter}
       end,
-      pythonPath = function(config)
-        return get_python_path(config.root_dir)
-      end,
+      -- pythonPath = function(config)
+      --   return get_python_path(config.root_dir)
+      -- end,
       -- args = {'test'}
     -- program = "-m pytest ${file}";
     },
