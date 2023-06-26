43486bef981026be830fa98e36483d8b940e3367
ceppes <romero.diego31@gmail.com>
2023-06-27 00:06:36 +0200

    add some hint for packer snapshot rollback sync preview

--- lua/features/packer.lua
+++ lua/features/packer.lua
@@ -3,6 +3,17 @@ local M = {}
 function M.keymap()
   packer = require('packer')
   vim.keymap.set('n', '<leader>ps', packer.sync, {desc = 'Packer Sync'})
+  vim.keymap.set('n', '<leader>psp', "<cmd>PackerSync --preview <cr>", {desc = 'Packer Sync Preview'})
+-- :PackerSnapshot snapshot-main
+-- :PackerSnapshotRollback snapshot-main
+
+-- 1 : packer sync preview
+-- 2 : packer sync
+-- if fail
+-- 3 : packer snapshot rollback snapshot-main
+-- if not :
+-- 4 : packer snapshot snapshot-main
+
 end
 
 function M.use(plugins)
