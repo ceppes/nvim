dcec8a6fa64623c2085a68c5b96acf9f7cc46b7c
ceppes <romero.diego31@gmail.com>
2023-08-25 18:12:18 +0200

    add custom icon compl

--- lua/features/lspconfig/setup.lua
+++ lua/features/lspconfig/setup.lua
@@ -5,6 +5,36 @@ local lsp_ensure_installed = {
   "tsserver"
 }
 
+local protocol = require("vim.lsp.protocol")
+protocol.CompletionItemKind = {
+  '', -- Text
+  '', -- Method
+  '', -- Function
+  '', -- Constructor
+  '', -- Field
+  '', -- Variable
+  '', -- Class
+  'ﰮ', -- Interface
+  '', -- Module
+  '', -- Property
+  '', -- Unit
+  '', -- Value
+  '', -- Enum
+  '', -- Keyword
+  '﬌', -- Snippet
+  '', -- Color
+  '', -- File
+  '', -- Reference
+  '', -- Folder
+  '', -- EnumMember
+  '', -- Constant
+  '', -- Struct
+  '', -- Event
+  'ﬦ', -- Operator
+  '', -- TypeParameter
+}
+
 require('mason').setup({ ui = { border = 'rounded' } })
 require("mason-lspconfig").setup{ ensure_installed = lsp_ensure_installed }
 require("fidget").setup{} -- little progress bar for lsp loading
+
