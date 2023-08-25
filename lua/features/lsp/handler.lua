3ae51aac51a6286a4e8ba2fbbb80f4d9cfd8c63f
ceppes <romero.diego31@gmail.com>
2023-08-25 18:13:12 +0200

    add publishdiagnostic handler

--- lua/features/lsp/handler.lua
+++ lua/features/lsp/handler.lua
@@ -5,6 +5,17 @@ M.hover = {
     vim.lsp.handlers.hover,
     {border = "rounded",}
   ),
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        prefix = "\u{ea71}"
+      },
+      severity_sort = true
+    }
+  )
 }
 
 return M
