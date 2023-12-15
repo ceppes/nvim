9ff0482511a34ed6c437a76dbffee3a834297ace
ceppes <romero.diego31@gmail.com>
2023-12-15 02:28:44 +0100

    add some hint for undercurl

--- lua/features/lspconfig/highlight.lua
+++ lua/features/lspconfig/highlight.lua
@@ -1,3 +1,10 @@
+-- Undercurl
+-- test : echo -e "\e[4:3mTEST"
+
+--tmux
+--set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+--set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+
 vim.cmd.highlight("DiagnosticUnderlineError guisp=red gui=undercurl")
 vim.cmd.highlight("DiagnosticUnderlineWarn guisp=orange gui=undercurl")
 vim.cmd.highlight("DiagnosticUnderlineInfo guisp=yellow gui=undercurl")
