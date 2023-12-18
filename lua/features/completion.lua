231df287503a01c0de9b9ac56be36f4c1c3ee57d
ceppes <romero.diego31@gmail.com>
2023-12-18 01:03:37 +0100

    add emoji to cmp

--- lua/features/completion.lua
+++ lua/features/completion.lua
@@ -8,6 +8,7 @@ M = {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-emoji',
     --  Snipper : Use luasnip
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
