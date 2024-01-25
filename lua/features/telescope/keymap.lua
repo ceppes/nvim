cac33d14d470ff746975a177d1db50423f6d7925
ceppes <romero.diego31@gmail.com>
2024-01-25 18:15:15 +0100

    add live grep hidden keymap

--- lua/features/telescope/keymap.lua
+++ lua/features/telescope/keymap.lua
@@ -14,12 +14,19 @@ require'which-key'.register({
 local builtin = require'telescope.builtin'
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'T Find files'} )
-vim.keymap.set('n', '<leader>feh', function ()
+vim.keymap.set('n', '<leader>fhf', function ()
     builtin.find_files({hidden = true})
   end,
   {desc = 'T Find files with hidden'} )
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "T Git files"})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = "T Live grep"})
+vim.keymap.set('n', '<leader>fhl', function ()
+    builtin.live_grep({
+      additional_args = function(_)
+        return { "--hidden" }
+      end
+    })
+  end, { desc = "T Live grep with hidden"})
 vim.keymap.set('n', '<leader>fs', function ()
     builtin.grep_string();
     -- builtin.grep_string({ search = vim.fn.input("Grep > ")});
