2e4cb9fe7974adc449c104d058cf8ca325690050
ceppes <romero.diego31@gmail.com>
2024-04-26 09:34:22 +0200

    move filetype config

--- lua/features/languages/shell.lua
+++ lua/features/languages/shell.lua
@@ -5,4 +5,17 @@ M.filetype = "sh"
 M.treesitter = "bash"
 M.formatter = "shfmt"
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "sh",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.foldmethod='indent'
+    vim.opt_local.expandtab = true
+  end
+})
+
+-- test
+-- :verbose set tabstop?
+
 return M
