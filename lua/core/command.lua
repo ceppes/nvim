10a10cb28d5b678782f9e848b87597fa91956d2d
ceppes <romero.diego31@gmail.com>
2025-02-12 22:40:18 +0100

    feat - Command : Add FT to print current filetype

--- lua/core/command.lua
+++ lua/core/command.lua
@@ -4,3 +4,6 @@ vim.api.nvim_create_user_command('Q', 'q', { nargs = 0 })
 vim.api.nvim_create_user_command('Wa', 'wa', { nargs = 0 })
 vim.api.nvim_create_user_command('Wq', 'wq', { nargs = 0 })
 
+vim.api.nvim_create_user_command('FT', function ()
+  print(vim.bo.filetype)
+end, { nargs = 0, desc = 'Print filetype' })
