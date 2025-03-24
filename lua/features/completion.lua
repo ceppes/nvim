2b2cb6770cef31b4565b6ef3e566c96df397d15e
ceppes <romero.diego31@gmail.com>
2025-03-24 11:25:13 +0100

    feat - Completion : Add bunch of source stolen from somewhere

--- lua/features/completion.lua
+++ lua/features/completion.lua
@@ -41,6 +41,15 @@ M = {
       --'hrsh7th/cmp-calc',
       --'f3fora/cmp-spell',
       "onsails/lspkind.nvim", -- vscodelike picto
+      "hrsh7th/cmp-omni",
+      "hrsh7th/cmp-nvim-lsp-document-symbol",
+      "petertriho/cmp-git",
+      "rafamadriz/friendly-snippets",
+      "rcarriga/cmp-dap",
+      "SergioRibera/cmp-dotenv",
+      "Snikimonkd/cmp-go-pkgs",
+      "David-Kunz/cmp-npm",
+      "davidsierradz/cmp-conventionalcommits",
     },
     config = function()
       require("features.completion").setup()
@@ -102,21 +111,142 @@ function M.setup()
   cmp.setup({
     completion = { completeopt = 'menu,menuone,noinsert,noselect' },
     preselect = cmp.PreselectMode.None,
-    sources = {
-      { name = "nvim_lsp", priority = 100 }, -- Keep LSP results on top.
-      { name = 'luasnip', priority = 75 },
+    sources = cmp.config.sources({
+      {
+        name = "nvim_lsp",
+        priority = 100,
+        group_index = 1,
+      },
+      {
+        name = "luasnip",
+        priority = 75,
+        group_index = 1,
+        option = { show_autosnippets = true, use_show_condition = false },
+      },
       { name = "path", priority = 50},
-      { name = "emoji",
+      { name = "spell" },
+      {
+        name = "nvim_lua",
+        entry_filter = function()
+          if vim.bo.filetype ~= "lua" then
+            return false
+          end
+          return true
+        end,
+        priority = 150,
+        group_index = 1,
+      },
+      {
+        name = "lazydev",
+        group_index = 0,
+        entry_filter = function()
+          if vim.bo.filetype ~= "lua" then
+            return false
+          end
+          return true
+        end,
+        priority = 100,
+      },
+      { name = "nvim_lsp_signature_help", priority = 100, group_index = 1 },
+      {
+        name = "treesitter",
+        max_item_count = 5,
+        priority = 90,
+        group_index = 5,
+        entry_filter = function(entry, vim_item)
+          if entry.kind == 15 then
+            local cursor_pos = vim.api.nvim_win_get_cursor(0)
+            local line = vim.api.nvim_get_current_line()
+            local next_char = line:sub(cursor_pos[2] + 1, cursor_pos[2] + 1)
+            if next_char == '"' or next_char == "'" then
+              vim_item.abbr = vim_item.abbr:sub(1, -2)
+            end
+          end
+          return vim_item
+        end,
+      },
+      {
+        name = "rg",
+        keyword_length = 5,
+        max_item_count = 5,
+        option = {
+          additional_arguments = "--smart-case --hidden",
+        },
+        priority = 80,
+        group_index = 3,
+      },
+      {
+        name = "buffer",
+        max_item_count = 5,
+        keyword_length = 2,
+        priority = 50,
+        entry_filter = function(entry)
+          return not entry.exact
+        end,
+        option = {
+          get_bufnrs = function()
+            return vim.api.nvim_list_bufs()
+          end,
+        },
+        group_index = 4,
+      },
+      {
+        name = "git",
+        entry_filter = function()
+          if vim.bo.filetype ~= "gitcommit" then
+            return false
+          end
+          return true
+        end,
+        priority = 40,
+        group_index = 5,
+      },
+      { name = "dap", priority = 40, group_index = 6 },
+      { name = "async_path", priority = 30, group_index = 5 },
+      { name = "calc", priority = 10, group_index = 9 },
+      {
+        name = "conventionalcommits",
+        priority = 10,
+        group_index = 9,
+        max_item_count = 5,
+        entry_filter = function()
+          if vim.bo.filetype ~= "gitcommit" then
+            return false
+          end
+          return true
+        end,
+      },
+      {
+        name = "fish",
+        priority = 10,
+        group_index = 9,
+        entry_filter = function()
+          if vim.bo.filetype ~= "gitcommit" then
+            return false
+          end
+          return true
+        end,
+      },
+      {
+        name = "emoji",
+        priority = 10,
+        group_index = 9,
+        entry_filter = function()
+          if vim.bo.filetype ~= "gitcommit" then
+            return false
+          end
+          return true
+        end,
         option = {
           insert = true
         }
       },
-      { name = 'nvim_lsp_signature_help' },
-      { name = "spell" },
-      { name = "treesitter" },
-      { name = "nvim_lua" },
-      { name = "buffer", priority = 1 }, -- Keep buffer words last
-    },
+      { name = "omni" },
+      { name = "nvim-lsp-document-symbol" },
+      { name = "dotenv" },
+      { name = "npm" },
+
+    }),
     snippet = {
       expand = function(args)
         luasnip.lsp_expand(args.body)
