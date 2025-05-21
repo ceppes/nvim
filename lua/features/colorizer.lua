f1dd57a84612f7354bf0d42e5a616202b3654753
ceppes <romero.diego31@gmail.com>
2025-05-21 17:43:45 +0200

    fix - colorizer :

--- lua/features/colorizer.lua
+++ lua/features/colorizer.lua
@@ -1,27 +1,27 @@
 local M = {}
 
 M = {
-  'norcalli/nvim-colorizer.lua',
-  config = function()
-    require("features.colorizer").setup()
-  end
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		require("features.colorizer").setup()
+	end,
 }
 
 function M.setup()
-  local colorizer_status_ok, colorizer = pcall(require, 'colorizer')
-  if not colorizer_status_ok then
-    return
-  end
+	local colorizer_status_ok, colorizer = pcall(require, "colorizer")
+	if not colorizer_status_ok then
+		return
+	end
 
-  colorizer.setup({
-    'css';
-    'scss';
-    'javascript';
-    'html';
-    'python';
-    'lua';
-    'ts';
-  })
+	colorizer.setup({
+		"css",
+		"scss",
+		"javascript",
+		"html",
+		"python",
+		"lua",
+		"ts",
+	})
 end
 
 return M
