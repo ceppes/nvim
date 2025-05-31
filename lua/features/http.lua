dfd8e4bb1fcf7b073d6ebcd8ac21750e5f9d0e0c
ceppes <romero.diego31@gmail.com>
2025-05-31 14:15:23 +0200

    fix - rest :

--- lua/features/http.lua
+++ lua/features/http.lua
@@ -1,23 +1,29 @@
 local M = {}
 
-M ={
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true,
-    opts = {
-      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
-    }
-  },
-  {
-    "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("rest-nvim").setup()
-      vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", {desc = 'Rest run'})
-    end,
-  }
+M = {
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+            rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+        },
+    },
+    {
+        "rest-nvim/rest.nvim",
+        -- https://github.com/rest-nvim/rest.nvim/tree/main/spec/examples
+        ft = "http, rest",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            opts = function(_, opts)
+                opts.ensure_installed = opts.ensure_installed or {}
+                table.insert(opts.ensure_installed, "http")
+            end,
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Rest run" })
+        end,
+    },
 }
 
 return M
