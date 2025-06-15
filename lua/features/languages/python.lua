b2126be2fc9994b8ce8064beb7c4ce726c1f256f
ceppes <romero.diego31@gmail.com>
2025-06-15 17:42:03 +0200

    style - python :

--- lua/features/languages/python.lua
+++ lua/features/languages/python.lua
@@ -37,46 +37,45 @@ function M.keymaps()
     end
 end
 
+local function get_python_path(workspace)
+    -- print("testtttt")
+    -- print("wod " .. workspace)
+    local util = require("lspconfig/util")
+    local path = util.path
+
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        print("1 " .. path.join(vim.env.VIRTUAL_ENV, "bin", "python"))
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+
+    -- Find and use virtualenv via poetry in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+    if match ~= "" then
+        local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+        print("2 " .. path.join(venv, "bin", "python"))
+        return path.join(venv, "bin", "python")
+    end
+
+    -- Find and use virtualenv in workspace directory.
+    for _, pattern in ipairs({ "*", ".*" }) do
+        local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+        if match ~= "" then
+            print("3 " .. path.join(path.dirname(match), "bin", "python"))
+            return path.join(path.dirname(match), "bin", "python")
+        end
+    end
+
+    -- Fallback to system Python.
+    local res = vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+    vim.notify("python path : " .. res)
+    return res
+end
+
 function M.lsp()
     local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
     if not lsp_status_ok then
         return
-    end
-
-    local util = require("lspconfig/util")
-    local path = util.path
-
-    local function get_python_path(workspace)
-        -- print("testtttt")
-        -- print("wod " .. workspace)
-
-        -- Use activated virtualenv.
-        if vim.env.VIRTUAL_ENV then
-            print("1 " .. path.join(vim.env.VIRTUAL_ENV, "bin", "python"))
-            return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-        end
-
-        -- Find and use virtualenv via poetry in workspace directory.
-        local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
-        if match ~= "" then
-            local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
-            print("2 " .. path.join(venv, "bin", "python"))
-            return path.join(venv, "bin", "python")
-        end
-
-        -- Find and use virtualenv in workspace directory.
-        for _, pattern in ipairs({ "*", ".*" }) do
-            local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
-            if match ~= "" then
-                print("3 " .. path.join(path.dirname(match), "bin", "python"))
-                return path.join(path.dirname(match), "bin", "python")
-            end
-        end
-
-        -- Fallback to system Python.
-        local res = vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
-        vim.notify("python path : " .. res)
-        return res
     end
 
     local python_root_files = {
