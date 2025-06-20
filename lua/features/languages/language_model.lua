fee4644a523b8b3a815436b56cee390ef6ccf0aa
ceppes <romero.diego31@gmail.com>
2025-06-20 23:57:52 +0200

    fix - langage model :

--- lua/features/languages/language_model.lua
+++ lua/features/languages/language_model.lua
@@ -1,11 +1,10 @@
-
 local M = {}
 
 M.lsp_key = ""
 M.lspbin = ""
 M.debugger = ""
 M.treesitter = {""}
-M.filhtype = { ""}
+M.filetypes = { ""}
 M.linter = {""}
 M.formatter = { "" }
 
@@ -16,6 +15,16 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 function M.lsp()
+    local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
+    if not lsp_status_ok then
+        return
+    end
+
+    return require("features.lsp.server_config").config({
+        cmd = {  },
+        filetypes = M.filetypes,
+        root_markers = lspconfig.util.root_pattern(),
+    })
 end
 
 function M.plugin()
@@ -23,9 +32,31 @@ end
 
 
 function M.keymaps()
+    vim.keymap.set("n", "<leader>", function() end, { desc = "" })
 end
 
 function M.debugger()
+    local dap_status_ok, dap = pcall(require, "dap")
+    if not dap_status_ok then
+        return
+    end
+
+    dap.adapters.language = {
+        type = "executable",
+        command = function(config)
+        end,
+        args = {  },
+    }
+
+
+    dap.configurations.language = {
+        {
+            name = "",
+            type = "language",
+            request = "launch",
+            program = "",
+        },
+    }
 end
 
 return M
