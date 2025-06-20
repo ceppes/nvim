0cf6740b9574a83ba7f716587e202a05d146166c
ceppes <romero.diego31@gmail.com>
2025-06-20 23:59:55 +0200

    refactor - lint :

--- lua/features/lint.lua
+++ lua/features/lint.lua
@@ -20,27 +20,29 @@ M = {
 }
 
 function M.get_active_clients()
-    -- Add linters (from nvim-lint)
     local buf_ft = vim.bo.filetype
     local buf_client_names = {}
+
     local lint_s, lint = pcall(require, "lint")
+    if not lint_s then
+        vim.notify("Failed to load the 'lint' module. Linting will not be available.", vim.log.levels.ERROR)
+        return
+    end
 
-    if lint_s then
-        if lint.get_running() == nil then
-            return { "No Lint" }
-        end
+    if lint.get_running() == nil then
+        return { "No Lint" }
+    end
 
-        for ft_k, ft_v in pairs(lint.linters_by_ft) do
-            if type(ft_v) == "table" then
-                for _, linter in ipairs(ft_v) do
-                    if buf_ft == ft_k then
-                        table.insert(buf_client_names, linter)
-                    end
-                end
-            elseif type(ft_v) == "string" then
+    for ft_k, ft_v in pairs(lint.linters_by_ft) do
+        if type(ft_v) == "table" then
+            for _, linter in ipairs(ft_v) do
                 if buf_ft == ft_k then
-                    table.insert(buf_client_names, ft_v)
+                    table.insert(buf_client_names, linter)
                 end
+            end
+        elseif type(ft_v) == "string" then
+            if buf_ft == ft_k then
+                table.insert(buf_client_names, ft_v)
             end
         end
     end
@@ -49,7 +51,8 @@ end
 
 function M.setup()
     local lint_ensure_installed = {}
-    local lint_ft_linter = {}
+    local lint_ensure_installed_unique = {}
+    local linter_by_ft = {}
 
     local servers = require("features.lspconfig.servers")
     local lint = require("lint")
@@ -58,46 +61,50 @@ function M.setup()
         if config.linter then
             if type(config.linter) == "table" then
                 for _, linter in ipairs(config.linter) do
-                    table.insert(lint_ensure_installed, linter)
+                    lint_ensure_installed_unique[linter] = true
                 end
             else
-                table.insert(lint_ensure_installed, config.linter)
+                lint_ensure_installed_unique[config.linter] = true
             end
         end
 
-        if config.linter and config.filetype then
+        -- Check if linter and filetype definitions exist in the config
+        if config.linter and config.filetypes then
+            -- Ensure linter is encapsulated in a table
             local linters = type(config.linter) == "table" and config.linter or { config.linter }
-            lint_ft_linter[config.filetype] = linters
 
-            for _, linter in ipairs(linters) do
-                if config.linter_cmd and not config.linter_cmd:find("/") and lint.linters[linter] then
-                    lint.linters[linter].cmd = config.linter_cmd
+            -- Process filetype list, assuming it can be a single string or a table of filetypes
+            local filetypes = type(config.filetypes) == "table" and config.filetypes or { config.filetypes }
+
+            for _, filetype in ipairs(filetypes) do
+                -- Initialize linter list for the filetype if it doesn't exist
+                if not linter_by_ft[filetype] then
+                    linter_by_ft[filetype] = {}
+                end
+
+                -- Add unique linters for each filetype
+                for _, linter in ipairs(linters) do
+                    if not vim.tbl_contains(linter_by_ft[filetype], linter) then
+                        table.insert(linter_by_ft[filetype], linter)
+                    end
+
+                    -- Optionally, configure the linter command if necessary
+                    if config.linter_cmd and not config.linter_cmd:find("/") and lint.linters[linter] then
+                        lint.linters[linter].cmd = config.linter_cmd
+                    end
                 end
             end
         end
     end
 
-    local unique = {}
-    for _, linter in ipairs(lint_ensure_installed) do
-        unique[linter] = true
+    for linter, _ in pairs(lint_ensure_installed_unique) do
+        table.insert(lint_ensure_installed, linter)
     end
 
-    local lint_unique = {}
-    for linter, _ in pairs(unique) do
-        table.insert(lint_unique, linter)
-    end
+    -- vim.print(linter_by_ft)
+    -- vim.print(lint_ensure_installed)
 
-    -- for k,v in pairs(lint_ft_linter) do
-    --   for i,j in pairs(v) do
-    --     print(k, v, i, j)
-    --   end
-    -- end
-
-    -- for i,j in pairs(lint_unique) do
-    --   print(i, j)
-    -- end
-
-    lint.linters_by_ft = lint_ft_linter
+    lint.linters_by_ft = linter_by_ft
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
         callback = function()
@@ -106,7 +113,7 @@ function M.setup()
     })
 
     require("mason-nvim-lint").setup({
-        ensure_installed = lint_unique,
+        ensure_installed = lint_ensure_installed_unique,
         automatic_installation = true,
     })
 end
