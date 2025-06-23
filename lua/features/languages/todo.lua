c2a0181ea54d2091948f9d04b406a457e75e1daa
ceppes <romero.diego31@gmail.com>
2025-06-23 16:40:43 +0200

    list of lsp to setup

---
+++ lua/features/languages/todo.lua
@@ -0,0 +1,90 @@
+-- buf -- protobuf
+-- pbls
+-- protolint
+-- protols
+
+-- checkstyle - java to test in comparison to other
+-- https://github.com/mfussenegger/nvim-lint/blob/master/lua/lint/linters/checkstyle.lua
+-- gradle-language-server
+
+-- clangd -- c++
+-- cpplint
+-- cpptools
+
+-- copilot-language-server
+
+-- csharp-language-server
+-- csharpier
+-- omnisharp
+-- omnisharp-mono
+
+-- css-variable-language-server
+-- cssmodules-language-server
+-- some-sass-language-server
+-- stylelint
+
+-- html-lsp
+-- htmlbeautifier
+-- htmlhint
+-- superhtml
+
+-- Python
+-- docformatter
+-- pyproject-fmt
+-- ruff
+
+-- toml
+-- tombi
+
+-- docker-compose-language-service
+-- dockerfile-language-server
+
+-- gh-actions-language-server
+-- gh
+-- gitlab-ci-ls
+
+-- grammarly-languageserver
+-- harper-ls
+-- proselint
+
+-- jq-lsp
+
+-- kube-linter
+
+--kulala-fmt -- .http .rest
+
+-- latexindent
+-- tex-fmt
+-- texlab
+-- textlint
+-- textlsp
+
+-- lemminx - xml lsp
+
+-- llm-ls
+
+--ltex-ls -- latex, markdown
+-- missing markdown lsp, lint etc
+
+-- luacheck -- lint
+-- luaformatter
+
+-- sql
+-- pgformatter
+-- postgrestools
+-- sql-formatter
+-- sqlfluff
+-- sqlfmt
+-- sqlls
+-- sqls
+-- sqruff
+
+-- swiftlint
+-- xcbeautifer
+
+-- terraform
+-- terraform-ls
+-- tfsec
+-- tflint
+
+-- trivy - open source security scanner
