4646d9c6edfa30101308dfc04534eb17b6e9e2e9
ceppes <romero.diego31@gmail.com>
2025-06-23 13:16:27 +0200

    feat - notify : add keymaps and better config

--- lua/features/ui.lua
+++ lua/features/ui.lua
@@ -6,6 +6,7 @@ M = {
         lazy = true,
         config = function()
             require("features.ui").setup()
+            require("features.ui").keymaps()
         end,
     },
     {
@@ -41,10 +42,31 @@ function M.setup()
     require("notify").setup({
         background_colour = "#000000",
         timeout = 6000, --5000 default
+
+        render = "wrapped-default",
+        stages = "fade_in_slide_out",
+        max_width = math.floor(vim.o.columns * 0.8), -- e.g., 80% of your screen
     })
     vim.notify = require("notify")
 
     require("telescope").load_extension("notify")
 end
 
+function M.keymaps()
+    local notify = require("notify")
+
+    -- Close last notification
+    vim.keymap.set("n", "<leader>nc", function()
+        notify.dismiss({ pending = false, silent = false })
+    end, { desc = "Close last notification" })
+
+    -- Close all notifications
+    vim.keymap.set("n", "<leader>na", function()
+        notify.dismiss({ silent = true, pending = true })
+    end, { desc = "Close all notifications" })
+
+    vim.keymap.set("n", "<leader>fN", function()
+        require("telescope").extensions.notify.notify()
+    end, { desc = "[F]ind [N]otificaton history" })
+end
 return M
