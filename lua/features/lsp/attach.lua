4f6ea6f0ee5a10233a0d8f94b5a8e077d64ac914
ceppes <romero.diego31@gmail.com>
2025-08-27 16:37:40 +0200

    fix(navic): only use first lsp when there is mulitple instance

--- lua/features/lsp/attach.lua
+++ lua/features/lsp/attach.lua
@@ -10,9 +10,13 @@ function M.attach(client, bufnr)
 
     if navic_ok then
         if client.server_capabilities.documentSymbolProvider then
-            navic.attach(client, bufnr)
-            navbuddy.attach(client, bufnr)
-            vim.api.nvim_buf_set_var(bufnr, "nvim_navic_attached", true)
+            -- Only attach navic to first LSP server to avoid conflicts
+            local navic_attached = pcall(vim.api.nvim_buf_get_var, bufnr, "nvim_navic_attached")
+            if not navic_attached then
+                navic.attach(client, bufnr)
+                navbuddy.attach(client, bufnr)
+                vim.api.nvim_buf_set_var(bufnr, "nvim_navic_attached", true)
+            end
         end
     end
 end
