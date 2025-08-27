727f26609443b20ed47f76a26d272878e3d40016
ceppes <romero.diego31@gmail.com>
2025-08-27 16:38:05 +0200

    fix(tailwind): add more conf

--- lua/features/languages/tailwindcss.lua
+++ lua/features/languages/tailwindcss.lua
@@ -2,29 +2,55 @@ local M = {}
 
 M.lsp_key = "tailwindcss"
 M.lspbin = "tailwindcss-language-server"
+M.filetypes = {
+    "ejs",
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+}
 
 function M.lsp()
     return require("features.lsp.server_config").config({
-        tailwindCSS = {
-            classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
-            includeLanguages = {
-                eelixir = "html-eex",
-                elixir = "phoenix-heex",
-                eruby = "erb",
-                heex = "phoenix-heex",
-                htmlangular = "html",
-                templ = "html",
+        filetypes = M.filetypes,
+        root_markers = {
+            "tailwind.config.js",
+            "tailwind.config.cjs",
+            "tailwind.config.mjs",
+            "tailwind.config.ts",
+            "postcss.config.js",
+            "postcss.config.cjs",
+            "postcss.config.mjs",
+            "postcss.config.ts",
+            "package.json",
+            "node_modules",
+        },
+        settings = {
+            tailwindCSS = {
+                classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+                includeLanguages = {
+                    eelixir = "html-eex",
+                    elixir = "phoenix-heex",
+                    eruby = "erb",
+                    heex = "phoenix-heex",
+                    htmlangular = "html",
+                    templ = "html",
+                },
+                lint = {
+                    cssConflict = "warning",
+                    invalidApply = "error",
+                    invalidConfigPath = "error",
+                    invalidScreen = "error",
+                    invalidTailwindDirective = "error",
+                    invalidVariant = "error",
+                    recommendedVariantOrder = "warning",
+                },
+                validate = true,
             },
-            lint = {
-                cssConflict = "warning",
-                invalidApply = "error",
-                invalidConfigPath = "error",
-                invalidScreen = "error",
-                invalidTailwindDirective = "error",
-                invalidVariant = "error",
-                recommendedVariantOrder = "warning",
-            },
-            validate = true,
         },
     })
 end
