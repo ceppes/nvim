f679d6ed86f06431c0a6e504f4298550e6efe790
ceppes <romero.diego31@gmail.com>
2025-12-01 23:54:05 +0100

    feat(yaml): improved schema

--- lua/features/languages/yaml.lua
+++ lua/features/languages/yaml.lua
@@ -45,22 +45,58 @@ function M.lsp()
                     url = "https://www.schemastore.org/api/json/catalog.json",
                 },
                 schemas = {
-                    kubernetes = "/*.y*ml", --globPattern
-                    ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+                    -- Ansible schemas (most specific patterns first)
+                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = {
+                        "playbook*.{yml,yaml}",
+                        "*playbook.{yml,yaml}",
+                        "site.{yml,yaml}",
+                    },
+                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = {
+                        "tasks/*.{yml,yaml}",
+                        "handlers/*.{yml,yaml}",
+                    },
+                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/requirements.json"] = {
+                        "requirements.{yml,yaml}",
+                        "roles/requirements.{yml,yaml}",
+                        "collections/requirements.{yml,yaml}",
+                    },
+                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/meta.json"] = {
+                        "meta/main.{yml,yaml}",
+                        "*/meta/main.{yml,yaml}",
+                    },
+                    ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/vars.json"] = {
+                        "vars/*.{yml,yaml}",
+                        "defaults/*.{yml,yaml}",
+                        "group_vars/*.{yml,yaml}",
+                        "host_vars/*.{yml,yaml}",
+                    },
+
+                    -- Kubernetes & Cloud Native
+                    ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = {
+                        "k3s/**/*.{yml,yaml}",
+                        "k8s/**/*.{yml,yaml}",
+                    },
                     ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                     ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
+                    ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+
+                    -- CI/CD
                     ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
                     ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.yml",
-                    ["http://json.schemastore.org/ansible-stable-2.9"] = "tasks/*.{yml,yaml}",
-                    ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-                    ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-                    ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-                    ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
                     ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.yml",
                     ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+
+                    -- Docker & Container
+                    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+
+                    -- Other
+                    ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                    ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                    ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+
+                    -- Fallback to Kubernetes for generic yaml files (commented out to avoid conflicts)
+                    -- kubernetes = "/*.y*ml",
                 },
                 format = {
                     enabled = true,
